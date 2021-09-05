7dd66501ed7620b99d4fe0707ac62040038c890a
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-05 17:09:56 +0000

    update of app title and place it into a safe are for avoiding collaps with task bar

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/constantes.dart';
 import 'package:onlineshop_flutter/size_config.dart';
 
 class Body extends StatefulWidget {
@@ -9,24 +10,33 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 3,
-          child: Column(
-            children: <Widget>[
-              Text(
-                'E-sugu',
-                style: TextStyle(fontSize: getProportionateScreenWidth(36))
-              )
-            ],
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Mali sugu',
+                  style: TextStyle(
+                      fontSize: getProportionateScreenWidth(36),
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+                Text(
+                  'Bienvenu à Mali sugu, le shoping en toute simplicité !',
+                )
+              ],
+            )
+          ),
+          Expanded(
+            flex: 2,
+            child: SizedBox()
           )
-        ),
-        Expanded(
-          flex: 2,
-          child: SizedBox()
-        )
-      ],
+        ],
+      ),
     );
   }
 }
