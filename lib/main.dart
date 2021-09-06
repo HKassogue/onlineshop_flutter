0ae9b75659d408a516a8370468717e0d80d5229a
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-06 13:49:34 +0000

    placing app them into a separated file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:onlineshop_flutter/constantes.dart';
 import 'package:onlineshop_flutter/routes.dart';
 import 'package:onlineshop_flutter/screens/splash/splash_screen.dart';
+import 'package:onlineshop_flutter/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,28 +18,6 @@ class MyApp extends StatelessWidget {
       //home: SplashScreen(),
       initialRoute: SplashScreen.routeName,
       routes: routes,
-    );
-  }
-
-  ThemeData theme() {
-    return ThemeData(
-      scaffoldBackgroundColor: Colors.white,
-      fontFamily: 'Muli',
-      appBarTheme: AppBarTheme(
-        color: Colors.white,
-        elevation: 0,
-        brightness: Brightness.light,
-        iconTheme: IconThemeData(color: Colors.black),
-        textTheme: TextTheme(
-            headline6: TextStyle(color: Color(0xFF8B8B8B), fontSize: 18)
-        )
-      ),
-      textTheme: TextTheme(
-        bodyText1: TextStyle(color: kTextColor),
-        bodyText2: TextStyle(color: kTextColor),
-      ),
-      //primarySwatch: Colors.blue,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
     );
   }
 }
