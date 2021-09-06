88ecc17d15d60b1267fb3fd61fcbd30b52e9b8a7
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-06 22:33:44 +0000

    placing email field into sign in screen

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/constantes.dart';
 
 import '../../../size_config.dart';
 
@@ -8,23 +9,67 @@ class Body extends StatelessWidget {
     return SafeArea(
       child: SizedBox(
         width: double.infinity,
-        child: Column(
-          children: [
-            Text(
-              "Bon retour",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: getProportionateScreenWidth(28),
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20)
+          ),
+          child: Column(
+            children: [
+              Text(
+                "Bon retour",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              "Connectez vous avec votre email\nou continuez avec un réseau social",
-              textAlign: TextAlign.center,
-            )
-          ],
+              Text(
+                "Connectez vous avec votre email\nou continuez avec un réseau social",
+                textAlign: TextAlign.center,
+              ),
+              SignForm()
+            ],
+          ),
         )
       ),
+    );
+  }
+}
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Email",
+              hintText: "Entrez votre adresse email",
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 42,
+                vertical: 20
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+                borderSide: BorderSide(color: kTextColor),
+                gapPadding: 10
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: BorderSide(color: kTextColor),
+                  gapPadding: 10
+              )
+            ),
+          )
+        ],
+      )
     );
   }
 }
