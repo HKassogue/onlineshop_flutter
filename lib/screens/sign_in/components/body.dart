35a5dcf6052cbff606127a3bdf8e8fdd67bdfb85
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-06 14:10:42 +0000

    placing sign in screen first part

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../../size_config.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text(
+              "Bon retour",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: getProportionateScreenWidth(28),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Connectez vous avec votre email\nou continuez avec un réseau social",
+              textAlign: TextAlign.center,
+            )
+          ],
+        )
+      ),
+    );
   }
 }
