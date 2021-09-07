263eaef7e8164ce8ed935664cbb7dd02f7e6ea13
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-07 11:49:42 +0000

    making social card as widget class

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -29,19 +29,39 @@ class Body extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               SignForm(),
-              Container(
-                padding: EdgeInsets.all(getProportionateScreenWidth(8)),
-                height: getProportionateScreenHeight(40),
-                width: getProportionateScreenWidth(40),
-                decoration: BoxDecoration(
-                  color: Color(0xFFF5F6F9),
-                  shape: BoxShape.circle
-                ),
-                child: SvgPicture.asset("assets/icons/facebook-2.svg"),
-              )
+              SocialCard(icon: "assets/icons/facebook-2.svg", press: (){})
             ],
           ),
         )
+      ),
+    );
+  }
+}
+
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key? key,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  final String icon;
+  final Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(getProportionateScreenWidth(8)),
+        height: getProportionateScreenHeight(40),
+        width: getProportionateScreenWidth(40),
+        decoration: BoxDecoration(
+          color: Color(0xFFF5F6F9),
+          shape: BoxShape.circle
+        ),
+        child: SvgPicture.asset(icon),
       ),
     );
   }
