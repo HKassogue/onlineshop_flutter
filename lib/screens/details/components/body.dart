f5125ec1950503e73c02e39bc97796e2a62185d7
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-12 18:28:17 +0000

    add of product images for product details screen

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/models/Product.dart';
+import 'package:onlineshop_flutter/screens/details/components/product_images.dart';
+
+import '../../../size_config.dart';
 
 class Body extends StatelessWidget {
+  final Product product;
+
+  const Body({Key? key, required this.product}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        SizedBox(height: getProportionateScreenHeight(20)),
+        ProductImages(product: product)
+      ]
+    );
   }
 }
