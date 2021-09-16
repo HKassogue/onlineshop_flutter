06ea625a8df2ac489bc6d1ca6a9e502e5710273e
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 15:09:58 +0000

    displaying one card of cart screen

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/models/Cart.dart';
+import 'package:onlineshop_flutter/screens/cart/components/cart_card.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        CartCard(cart: demoCarts[0],)
+      ],
+    );
   }
 }
