a86942ad9d81c40747236b363bdb937ce0ce5eb0
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 18:20:41 +0000

    add of profil pic for profil screen

--- lib/screens/profile/components/body.dart
+++ lib/screens/profile/components/body.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/screens/profile/components/profile_pic.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          ProfilePic(),
+        ],
+      )
+    );
   }
 }
