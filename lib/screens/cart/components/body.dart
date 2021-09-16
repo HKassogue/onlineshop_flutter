12983ef5a30474bd615beeaa414cf0c2b8f23de9
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 15:59:22 +0000

    diplaying all cards for cart screen

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -2,31 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:onlineshop_flutter/models/Cart.dart';
 import 'package:onlineshop_flutter/screens/cart/components/cart_card.dart';
+import 'package:onlineshop_flutter/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Dismissible(
-          key: Key(demoCarts[0].product.id.toString()),
-          direction: DismissDirection.endToStart,
-          background: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-              color: Color(0xFFFFE6E6),
-              borderRadius: BorderRadius.circular(15),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ListView.builder(
+          itemCount: demoCarts.length,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Dismissible(
+                key: Key(demoCarts[index].product.id.toString()),
+                direction: DismissDirection.endToStart,
+                background: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFE6E6),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      SvgPicture.asset("assets/icons/Trash.svg"),
+                    ],
+                  ),
+                ),
+                child: CartCard(cart: demoCarts[index])
             ),
-            child: Row(
-              children: [
-                Spacer(),
-                SvgPicture.asset("assets/icons/Trash.svg"),
-              ],
-            ),
-          ),
-          child: CartCard(cart: demoCarts[0])
-        ),
-      ],
+          )
+      ),
     );
   }
 }
