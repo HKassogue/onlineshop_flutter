58160791f5082858d6b96ef329b652d889aa4524
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 14:59:05 +0000

    add of app bar for cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/models/Cart.dart';
 
 import 'components/body.dart';
 
@@ -7,7 +8,26 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(context),
       body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Column(
+        children: [
+          Text(
+            "Votre panier",
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            "${demoCarts.length} articles",
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ],
+      ),
     );
   }
 }
