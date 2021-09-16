a2406a70b7980bad65f31dcb76daf42ba520adb8
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 15:16:44 +0000

    making dismissible one card of cart screen

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:onlineshop_flutter/models/Cart.dart';
 import 'package:onlineshop_flutter/screens/cart/components/cart_card.dart';
 
@@ -7,7 +8,24 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CartCard(cart: demoCarts[0],)
+        Dismissible(
+          key: Key(demoCarts[0].product.id.toString()),
+          direction: DismissDirection.endToStart,
+          background: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+              color: Color(0xFFFFE6E6),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                Spacer(),
+                SvgPicture.asset("assets/icons/Trash.svg"),
+              ],
+            ),
+          ),
+          child: CartCard(cart: demoCarts[0])
+        ),
       ],
     );
   }
