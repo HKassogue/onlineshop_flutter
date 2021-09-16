ae0236bd6f4fbc6e43ce4c2e4103ce647f9348bd
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-16 18:26:32 +0000

    add of menus for profil screen

--- lib/screens/profile/components/body.dart
+++ lib/screens/profile/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshop_flutter/screens/profile/components/profile_menu.dart';
 import 'package:onlineshop_flutter/screens/profile/components/profile_pic.dart';
+import 'package:onlineshop_flutter/screens/splash/splash_screen.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -9,6 +11,32 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+            text: "Mon compte",
+            icon: "assets/icons/User Icon.svg",
+            press: () => {},
+          ),
+          ProfileMenu(
+            text: "Notifications",
+            icon: "assets/icons/Bell.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Paramètres",
+            icon: "assets/icons/Settings.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Centre d'aide",
+            icon: "assets/icons/Question mark.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Déconnexion",
+            icon: "assets/icons/Log out.svg",
+            press: () => Navigator.pushNamed(context, SplashScreen.routeName),
+          ),
         ],
       )
     );
